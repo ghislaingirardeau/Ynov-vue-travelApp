632842ccd5feaf6fc84538c82cc979d7100581be
ghislaingirardeau <g.girardeaupro@sfr.fr>
2024-10-07 10:53:12 +0200

    base home

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
-<script setup></script>
+<script setup>
+import sourceData from '@/assets/data.json'
+
+const destinations = sourceData.destinations
+</script>
 
 <template>
-  <main>hello</main>
+  <div class="container">
+    <main class="home">
+      <h1>destination</h1>
+      <div class="destinations">
+        <div v-for="destination in destinations" :key="destination.id">
+          <h2>
+            {{ destination.name }}
+          </h2>
+          <img :src="`/images/${destination.image}`" />
+        </div>
+      </div>
+    </main>
+  </div>
 </template>
 
 <style scoped></style>
